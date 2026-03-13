14467b180833b66f8624f7547309814183da0276
wilfredo-marapacuto <wmarapacutoverastegu@byupathway.edu>
2026-03-13 10:24:43 -0600

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Physical Therapist";
+        job1._company = "Central Hospital";
+        job1._startYear = 2010;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Business Consultant";
+        job2._company = "WM Servicios Hispanos";
+        job2._startYear = 2023;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Wilfredo Marapacuto";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
